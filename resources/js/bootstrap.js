545794a1de96507adb16172707fc660b31c931cd
= <=>
2025-03-09 13:00:59 -0300

    correcao

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -12,6 +12,9 @@ window.$ = $;
 //AXIOS
 let token = document.head.querySelector('meta[name="csrf-token"]');
 axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+axios.defaults.headers.common['Cache-Control'] = 'no-cache, no-store, must-revalidate';
+axios.defaults.headers.common['Pragma'] = 'no-cache';
+axios.defaults.headers.common['Expires'] = '0';
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
