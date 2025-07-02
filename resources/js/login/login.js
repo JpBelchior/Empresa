886bcc6097aa0e3708012b208ec7e9f7687e3322
= <=>
2025-07-01 22:00:59 -0300

    correcao

--- resources/js/login/login.js
+++ resources/js/login/login.js
@@ -5,7 +5,7 @@ $("#btn_modal_recuperar_senha").click(() => { abrir_modal('modal_recuperar_senha
 $("#entrar").click(function(){
     habilitar_botao("entrar", false);
     let dados = { email: $("#email").val(), senha: $("#senha").val() };
-    axios.post('verificar_login', dados)
+    axios.post(app_url+'/verificar_login', dados)
     .then(response => {        
         location.reload();
     })
@@ -20,7 +20,7 @@ $("#entrar").click(function(){
 
 $("#btn_recuperar_senha").click(function(){
     habilitar_botao("btn_recuperar_senha", false);
-    axios.post('recuperacao_senha/solicitar', { cpf_cnpj: $("#cpf_cnpj").val() })
+    axios.post(app_url+'/recuperacao_senha/solicitar', { cpf_cnpj: $("#cpf_cnpj").val() })
     .then(response => {
         sucesso(response);
     })
