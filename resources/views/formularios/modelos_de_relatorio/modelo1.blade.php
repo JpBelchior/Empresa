840fa168cb95d05aab4b9c5330f6840619369db1
= <=>
2025-07-25 01:42:24 -0300

    correcao

--- resources/views/formularios/modelos_de_relatorio/modelo1.blade.php
+++ resources/views/formularios/modelos_de_relatorio/modelo1.blade.php
@@ -46,8 +46,9 @@ $respostas = $dados_modelo['respostas'];
 
     @media print and (max-width: 768px) {
       .quebra-pagina, .quebra-pagina-normal {
+        min-height: auto !important;
         page-break-after: avoid !important;
-        overflow: hidden !important;
+        
       }
     }
 
